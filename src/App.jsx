c3ffd71718b00696e5d9b4a31cdb556aada06f28
Ralph Vente <venteralph@gmail.com>
2019-06-05 14:19:41 -0400

    add styling

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import AppRouter from './components/navbar.jsx';
+import './components/navbar.css';
+
+import Card from './components/CityCard.jsx';
+import './components/CityCard.css';
+
+function App() {
+  return (
+      <div>
+        <AppRouter/>
+    <div className="card-container">
+      <Card LocationText="New York" Long="100" Lat="000" />
+    </div>
+      </div>
+  );
+}
+
+export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,12 @@ function Index() {
   return <h2>Home</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
+function City() {
+  return <h2>City Search</h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function ZipCode() {
+  return <h2>Zip-Code Search</h2>;
 }
 
 function AppRouter() {
@@ -23,17 +23,17 @@ function AppRouter() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about/">About</Link>
+              <Link to="/City/">City</Link>
             </li>
             <li>
-              <Link to="/users/">Users</Link>
+              <Link to="/ZipCode/">ZipCode</Link>
             </li>
           </ul>
         </nav>
 
         <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
+        <Route path="/City/" component={City} />
+        <Route path="/ZipCode/" component={ZipCode} />
       </div>
     </Router>
   );
