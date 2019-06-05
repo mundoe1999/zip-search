24bf57356f710b9b5a1298db7e81a986cb611724
mundoe1999 <estebanmundo1999@gmail.com>
2019-06-05 10:46:20 -0400

    Folder re-arraging

---
+++ src/components/zipcode.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function AppRouter() {
+
+}
+
+export default AppRouter;
+
